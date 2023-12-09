675ec60b6142f066b99ccbbe1b836835874c1672
Paraccoli_xX <m.mirim1357@gmail.com>
2023-12-09 21:43:27 +0900

    Bot_Template.py

--- Bot_Template.py
+++ Bot_Template.py
@@ -1,12 +1,25 @@
+#ライブラリ
 import discord
 from discord.ext import commands
+import logging
+from config import admin_account_id
+
+
+#一般ライブラリ
+import json
+import time
+import random
+import datetime
+import re
+
+#音声ライブラリ
 from gtts import gTTS
 import pyttsx3
 import asyncio
 import os
 from dotenv import load_dotenv
 
-load_dotenv("YOUR_.env_FILE_PASS_HERE")
+load_dotenv(".env")
 
 TOKEN = os.getenv('TOKEN')
 
@@ -20,22 +33,94 @@ emoji_pyttsx3 = "🇵"
 if TOKEN is None:
     raise ValueError('.envfileにTOKENがありません')
 
-client = commands.Bot(command_prefix='/')
+client = commands.Bot(command_prefix='/', description = "This bot is made by Paraccoli", ower_id = 957169126990827520)
+
+# ログファイルのパス
+log_file_path = "log.txt"
+
+# ログの設定
+logging.basicConfig(filename=log_file_path, level=logging.DEBUG)
+
+
+#Loginfo
+@client.slash_command(description="このコマンドは管理者専用です！")
+async def loginfo(ctx):
+    # ログチャンネルのIDを指定
+    log_channel_id = 1181620524774850761
+    log_channel = client.get_channel(log_channel_id)
+
+    sender_id = ctx.author.id
+
+    # 特定のアカウントIDと比較
+    if sender_id == admin_account_id:
+        if log_channel:
+            try:
+                with open(log_file_path, 'rb') as file:
+                    await log_channel.send(file=discord.File(file, 'log.txt'))
+            except FileNotFoundError:
+                print("ログファイルが見つかりません。")
+        else:
+            print("ログチャンネルが見つかりません。")
+    else:
+        embed = discord.Embed(title="デバックログ", description="ログを表示します。", color=discord.Color.green())
+        embed.add_field(name="/loginfo",value="あなたは管理者ではありません。このコマンドは管理者のみ使用できます。", inline=False)
+        await ctx.respond(embed=embed)
+
 
 #ログイン
 @client.event
 async def on_ready():
     print("on_ready")
     # play Project DIVA
-    await client.change_presence(activity=discord.Game("SampleGame"))
+    await client.change_presence(activity=discord.Game("Project DIVA"))
+
+    # ログの設定
+    logging.basicConfig(level=logging.DEBUG)  # DEBUG レベル以上のログを表示
+
+    # デバッグメッセージ
+    logging.debug("on_ready")
+
+    # ボット情報の表示
+    print(f'-----------------------------')
+    print(f'完了しました。')
+    print(f"{client.user.name}にログインしました。")
+    print(f"ID: {client.user.id}")
+    print(f"サーバー数: {len(client.guilds)}")
+    print(f"参加メンバー数: {len(client.users)}")
+    print(f'-----------------------------')
+
+    # インフォメーションメッセージ
+    logging.info("ログインしました。")
+    logging.info(f"ID: {client.user.id}")
+    logging.info(f"サーバー数: {len(client.guilds)}")
+    logging.info(f"参加メンバー数: {len(client.users)}")
+
+    # ワーニングメッセージ
+    logging.warning("これは警告メッセージです")
+
+    # エラーメッセージ
+    logging.error("これはエラーメッセージです")
+
+    # 終了時のログメッセージ
+    logging.info("Received signal to terminate bot and event loop.")
+    logging.info("Cleaning up tasks.")
+    logging.info("Cleaning up after 1 tasks.")
+    logging.info("All tasks finished cancelling.")
+    logging.info("Closing the event loop.")
+
 
 
 #名前を呼んで挨拶
 @client.slash_command(description="このコマンドはあなたの名前を呼んであいさつします！")
 async def name_hello(ctx, name):
-    await ctx.respond(name + "さんこんにちはー")
-
-
+     if name == "Paraccoli": 
+        embed = discord.Embed(title= name +"さんこんにちは！", description="デバックLOGを表示します。", color=discord.Color.blue())
+        embed.add_field(name="/loginfo",value="", inline=False)
+        await ctx.respond(embed=embed)
+     else:
+        embed = discord.Embed(title= name +"さんこんにちは！", description="私の使い方を知りたいですか？", color=discord.Color.green())
+        embed.add_field(name="/hello", value="このコマンドで使い方を説明しますよ！", inline=False)
+        await ctx.respond(embed=embed)
 
 #使い方の説明
 @client.slash_command(description="このコマンドはこのBotの使い方を説明します！\n使用方法：/hello")
@@ -47,6 +132,8 @@ async def hello(ctx):
     embed.add_field(name="/dictionary", value="単語を辞書に登録するよ！\n使用方法：/dictionary [単語] [読み方]", inline=False)
     embed.add_field(name="/dictionary_list", value="辞書に登録された単語一覧を表示するよ！", inline=False)
     embed.add_field(name="/member_list", value="ボイスチャンネル内のメンバー一覧を表示するよ！", inline=False)
+    embed.set_thumbnail(url='https://image.gif')
+    embed.set_image(url='https://giffiles.url')
     await ctx.respond(embed=embed)
 
 
